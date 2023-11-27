6a5632e93284ab77bfa450a4fc442f12d84c2904
Marcos Portela <marcosnasp@gmail.com>
2023-11-27 18:58:28 -0300

    Normalizando

--- projeto-computacional-filtro/gerador_audio.py
+++ projeto-computacional-filtro/gerador_audio.py
@@ -10,17 +10,18 @@ file_path = r'C:\Users\Marcos Portela\Documents\Desenv\repository\github\trabalh
 try:
     sample_rate, signal = wavfile.read(file_path)
 
-    W = 1200  # Largura do pulso em Hz
-    T = 1 / W  # Período do pulso
+    W = 1800 # Largura do pulso em Hz
+    T = 1 / W # Período do pulso
 
-    n = np.arange(0, 60 * sample_rate)  # Considerando apenas os primeiros 60 segundos
-    h = np.sinc(2 * W * (n / sample_rate - T / 2))  # Pulso retangular usando sinc function
+    print(f'Passagem para np.arange: {60 * sample_rate}')
+    n = np.arange(0, 60 * sample_rate) # Considerando apenas os primeiros 60 segundos
+    h = np.sinc(2 * W * (n / sample_rate - T / 2)) # Pulso retangular usando sinc function
 
-    print(f'Width of the pulse: {W}')
-    print(f'Period of the pulse: {T}')
-    print(f'Length of Signal: {len(signal)}')
-    print(f'Number of channels: {signal.shape[0]}')
-    print(f'Sample Rate: {sample_rate}')
+    print(f'Largura do Pulso em Hz: {W} Hz')
+    print(f'Período do Pulso: {T} Segundos')
+    print(f'Comprimento do Sinal: {len(signal)}')
+    print(f'Número de Canais: {signal.shape[0]}')
+    print(f'Taxa de Amostragem em Hz: {sample_rate}')
 
     # Calcular a resposta do filtro 𝑦[𝑛] usando o método da convolução no tempo
     y_conv = convolve(signal, h, mode='same')
@@ -36,21 +37,19 @@ try:
     y_ifft_freq = ifft(Y_freq)
 
     # Ajustar a escala para valores inteiros de 16 bits (PCM)
-    y_ifft_int16 = np.int16(y_ifft_freq.real / np.max(np.abs(y_ifft_freq.real)) * 32767)
+    y_norm = y_conv / W
+    y_norm_int16 = np.int16(y_norm.real / np.max(np.abs(y_norm.real)) * 32767)
 
-    print(f'Time-domain Convolution: {y_conv}')
-    print(f'Frequency-domain Convolution Spectrum: {np.abs(Y_conv)}')
-    print(f'Time-domain Frequency Multiplication: {y_freq}')
-    print(f'Frequency-domain Frequency Multiplication Spectrum: {np.abs(Y_freq)}')
-
-    # Calcular a resposta do filtro 𝑦[𝑛] usando o método da convolução no tempo
-    y_conv = convolve(signal, h, mode='same')
+    print(f'Convolução no domínio do Tempo: {y_conv}')
+    print(f'Espectro de convolução no Domínio da Frequência: {np.abs(Y_conv)}')
+    print(f'Multiplicação da Frequência no Domínio do Tempo: {y_freq}')
+    print(f'Espectro de Multiplicação de Frequência no Domínio do Tempo: {np.abs(Y_freq)}')
 
     # Escrever o sinal processado em um arquivo .wav
-    write('y_conv.wav', sample_rate, np.int16(y_conv.real))
+    write(f'convolucionado_{W}.wav', sample_rate, np.int16(y_conv.real))
 
     # Escrever o sinal processado em um arquivo .wav
-    write('y_filtered.wav', sample_rate, y_ifft_int16)
+    write(f'filtrado_normalizado_{W}.wav', sample_rate, y_norm_int16)
 
 except Exception as e:
     print(f"An error occurred: {e}")
